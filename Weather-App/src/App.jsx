ef9ab13b8735e4c8b03100d71f07e3e2b4da0293
shemanto27 <shemantosharkarofficial@gmail.com>
2024-10-20 02:18:04 +0600

    done

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import sun from './images/sun.webp';
+import thunder from './images/thunder.webp';
+import tornado from './images/Tornado.webp';
+import rain from './images/rain_with_cloud.webp';
 
 function App() {
 
@@ -6,7 +10,7 @@ function App() {
   const [location, setLocation] = useState('Dhaka');
   const [temp, setTemp] = useState('25.6 °C');
   const [weatherCondition, setWeatherCondition] = useState('cloud');
-  const [icon, setIcon] = useState('src\assets\sun.webp');
+  const [icon, setIcon] = useState(sun);
   
   // API data fetching
   const apiKey = 'd5757c464d1aed3a3d5cbafe849a267e';
@@ -20,24 +24,30 @@ function App() {
       setTemp(`${data.main.temp} °C`);
       setWeatherCondition(data.weather[0].description);
 
-      if(weatherCondition === 'clear sky'){
-        setIcon = 'src\assets\rain_with_cloud.webp';
-        console.log(icon);
+      if(weatherCondition == 'haze'){
+        setIcon(sun);
       }
-
-
-
+      else if(weatherCondition == 'clear sky'){
+        setIcon(sun);
+      }
+      else if(weatherCondition == 'broken clouds'){
+        setIcon(thunder);
+      }
+      else{
+        setIcon(rain);
+      }
     } 
     catch (error) {
       console.log(error);
     }
-  }
+  };
 
   function setData(e){
     e.preventDefault();
 
     fetchWeatherData();
-  }
+  };
+
 
 
  // Current date 
